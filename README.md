648fa2cc7d9e9ad5fd8c489cab227cb75d57f49d
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2023-06-24 19:26:39 +0330

    Change and save mp3 tags

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # mp3-tag-editor
 
-https://eyed3.readthedocs.io
-
 ```
 pip install eyeD3
 pip install python-magic-bin
 ```
+
+## Resources
+
+- [eyeD3](https://eyed3.readthedocs.io)
+- [How to iterate over files in directory using Python?](https://www.geeksforgeeks.org/how-to-iterate-over-files-in-directory-using-python/)
+- [How to Get File Extension in Python](https://www.digitalocean.com/community/tutorials/get-file-extension-in-python)
--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 import eyed3
+import os
+import re
 
-audiofile = eyed3.load("music.mp3")
+music_dir = "musics"
 
-print(audiofile.tag.title)
-print(audiofile.tag.track_num)
-print(audiofile.tag.artist)
-print(audiofile.tag.album)
-print(audiofile.tag.album_artist)
+# iterate over files in
+# that directory
+for filename in os.listdir(music_dir):
+    f = os.path.join(music_dir, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        # checking if it is mp3
+        _, file_extension = os.path.splitext(f)
+        if file_extension.lower() == ".mp3":
+            audiofile = eyed3.load(f)
+            audiofile_name = os.path.basename(audiofile.path)
 
-# audiofile.tag.title = "The Edge"
-# audiofile.tag.track_num = 3
-# audiofile.tag.artist = "Token Entry"
-# audiofile.tag.album = "Free For All Comp LP"
-# audiofile.tag.album_artist = "Various Artists"
+            regex_pattern = r"^(?P<artist>[^-]+)\s*-\s*(?P<title>[^-]+)\.[^.]+$"
+            matches = re.search(regex_pattern, audiofile_name)
+            if matches:
+                artist = matches.group("artist").strip()
+                title = matches.group("title").strip()
+                # change mp3 tags
+                audiofile.tag.album_artist = artist
+                audiofile.tag.title = title
+                audiofile.tag.artist = artist
 
-# audiofile.tag.save()
-
+                print("===========================================")
+                try:
+                  audiofile.tag.save()
+                except:
+                  print("*************************************")
+                  print("* An error occurred saving MP3 tags *")
+                  print("*************************************")
+                else:
+                  print(f"File Name: {audiofile_name}")
+                  print("------------------------------------------")
+                  print(f"Old Album Artist: {audiofile.tag.album_artist}")
+                  print(f"Old Title: {audiofile.tag.title}")
+                  print(f"Old Artist: {audiofile.tag.artist}")
+                  # print(f"Old Album: {audiofile.tag.album}")
+                  print("------------------------------------------")
+                  print(f"New Album Artist: {artist}")
+                  print(f"New Title: {title}")
+            else:
+                print("*************************************")
+                print("*         No matches found          *")
+                print("*************************************")
